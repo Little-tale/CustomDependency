8493f7425192938f3674bf29556922dd100fca7a
Kim JaeHyung <you3192001@icloud.com>
2024-11-06 17:57:12 +0900

    Initial Commit

---
+++ CustomDependency/CustomDependencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomDependencyApp.swift
+//  CustomDependency
+//
+//  Created by Jae hyung Kim on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomDependencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
